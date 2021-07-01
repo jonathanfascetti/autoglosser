3a3c440dca9c3fed5a0d39ed9f2a6aa427a23e2b
Jonathan Fascetti <jlfascetti@gmail.com>
2021-06-30 23:27:59 -0400

    - reorganized structure

--- cgi-bin/spreadtest.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get(
-    "https://docs.google.com/spreadsheets/d/1hht0h0BP-TeO_RHx07RF0UjK2VX-tcRU47bQ9FKS8Cw/export?gid=260382663&format=csv"
-)
-assert response.status_code == 200, "Wrong status code"
