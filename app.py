93fef13b800619c1045ffb0e695a91cf30b555d9
Marco Raigoza <mraigoza48@gmail.com>
2022-05-01 13:32:03 -0400

    rework files

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
-from api.HelloApiHandler import HelloApiHandler
+from website.api.HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__, static_url_path='', static_folder='website/frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
 
